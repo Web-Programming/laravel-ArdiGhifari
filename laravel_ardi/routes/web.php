fdcce90aa5311b4f81adae5fc72693538ff5e150
ArdiGhifari <106002679+ArdiGhifari@users.noreply.github.com>
2023-11-08 13:14:17 +0700

    Validasi Form

--- laravel_ardi/routes/web.php
+++ laravel_ardi/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\ProdiController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -99,7 +100,6 @@ Route::get('/mahasiswa/update', [MahasiswaController::class, 'update']);
 Route::get('/mahasiswa/delete', [MahasiswaController::class, 'delete']);
 Route::get('/mahasiswa/select', [MahasiswaController::class, 'select']);
 
-
 Route::get('/mahasiswa/insert-qb', [MahasiswaController::class, 'insertQb']);
 Route::get('/mahasiswa/update-qb', [MahasiswaController::class, 'updateQb']);
 Route::get('/mahasiswa/delete-qb', [MahasiswaController::class, 'deleteQb']);
@@ -109,3 +109,6 @@ Route::get('/mahasiswa/insert-elq', [MahasiswaController::class, 'insertElq']);
 Route::get('/mahasiswa/update-elq', [MahasiswaController::class, 'updateElq']);
 Route::get('/mahasiswa/delete-elq', [MahasiswaController::class, 'deleteElq']);
 Route::get('/mahasiswa/select-elq', [MahasiswaController::class, 'selectElq']);
+
+Route::post('/prodi/store', [ProdiController::class,'store']);
+Route::get('/prodi/create', [ProdiController::class,'create']);
